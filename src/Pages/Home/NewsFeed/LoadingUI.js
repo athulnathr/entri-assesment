65c60284e67448db16af0d910894c085a00d34b2
Athul Nath <id4athul@gmail.com>
2021-09-05 14:15:01 +0530

    LoadingUI Added

--- src/Pages/Home/NewsFeed/LoadingUI.js
+++ src/Pages/Home/NewsFeed/LoadingUI.js
@@ -1,7 +1,50 @@
 import React from "react";
 
 const LoadingUI = () => {
-  return [...new Array(12)].map(() => <div className="loading">Loading</div>);
+  return [...new Array(12)].map(() => (
+    <div className="col-sm-6 col-lg-3 col-xs-12">
+      <div className="single-feed">
+        <div className="image-holder">
+          <div
+            className="placeholder"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
+        <div className="content p-h">
+          <div className="d-flex jsb ac bottom-detail mb-h">
+            <div
+              className="placeholder"
+              style={{ width: "8rem", height: "1.2rem" }}
+            />
+            <div
+              className="placeholder"
+              style={{ width: "8rem", height: "1.2rem" }}
+            />
+          </div>
+          <div
+            className="placeholder"
+            style={{ width: "100%", height: "1.4rem", marginBottom: "0.4rem" }}
+          />
+          <div
+            className="placeholder"
+            style={{ width: "40%", height: "1.4rem", marginBottom: "0.4rem" }}
+          />
+          <div className="excerpt">
+            {[...new Array(4)]?.map(() => (
+              <div
+                className="placeholder"
+                style={{
+                  width: "100%",
+                  height: "0.75rem",
+                  marginBottom: "0.25rem",
+                }}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
 };
 
 export default LoadingUI;
